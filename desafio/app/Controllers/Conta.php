3b2787e1bcf6faf5313899925554764c88578721
Lucas Monteiro <64096755+lucasmmcota@users.noreply.github.com>
2021-09-29 18:46:26 -0300

    Update Conta.php

--- desafio/app/Controllers/Conta.php
+++ desafio/app/Controllers/Conta.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 class Conta extends BaseController
 {
+    // Mostra o número de todas as contas criadas
     public function index()
     {
         $contaModel = new \App\Models\ContaModel();
@@ -13,11 +14,12 @@ class Conta extends BaseController
         }
     }
 
+    // Retorna o valor da cotação de venda de uma determinada moeda
     public function cotacao($moeda)
     {
         $format = 'json';
         date_default_timezone_set('America/Sao_Paulo');
-        $data = date('m-d-Y', strtotime("-1 days"));
+        $data = date('m-d-Y', strtotime("-1 days")); // Dia de fechamento anterior
         $ch = curl_init("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='$moeda'&@dataCotacao='$data'&$format=json");
 
         curl_setopt_array($ch, [
@@ -39,6 +41,7 @@ class Conta extends BaseController
         return $valores["cotacaoVenda"];
     }
 
+    // Retorna um array com as moedas encotradas na API do Banco Central
     public function cadastrarMoedas()
     {
         $ch = curl_init("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?format=json");
@@ -62,6 +65,7 @@ class Conta extends BaseController
         return $resultado;
     }
 
+    // Verifica se uma determinada conta foi cadastrada
     public function acharConta($numeroDaConta, $numContas)
     {
         if ($numeroDaConta > 0 and $numeroDaConta <= $numContas) {
@@ -71,6 +75,7 @@ class Conta extends BaseController
         }
     }
 
+    // Cadastrar uma nova conta
     public function cadastrar()
     {
         $contaModel = new \App\Models\ContaModel();
@@ -79,6 +84,7 @@ class Conta extends BaseController
         $valores = $this->cadastrarMoedas();
         $moedas = "BRL";
         $saldo = "0";
+        // Preenchendo uma string de moedas e a outra do saldo em cada uma delas
         for ($i = 0; $i < 10; $i++) {
             $valor = $valores["value"][$i];
             $moedas = $moedas . " " . $valor["simbolo"];
@@ -96,6 +102,7 @@ class Conta extends BaseController
         }
     }
 
+    // Verifica se a cotação é maior ou menor do que o BRL
     public function verificarCotacao($cotacao)
     {
         if ($cotacao >= 1) {
@@ -105,6 +112,7 @@ class Conta extends BaseController
         }
     }
 
+    // Cadastra uma nova transação
     public function realizarTransacao($numeroDaConta, $valor, $moeda, $tipo)
     {
         $transacaoModel = new \App\Models\TransacaoModel();
@@ -119,7 +127,7 @@ class Conta extends BaseController
         $transacaoModel->set('valor', $valor);
         $transacaoModel->set('moeda', $moeda);
         date_default_timezone_set('America/Sao_Paulo');
-        $data = date("Y-m-d");
+        $data = date("Y-m-d"); // Modelo utilizado pelo MySQL
         $transacaoModel->set('data', $data);
 
         if ($transacaoModel->insert()) {
@@ -129,6 +137,7 @@ class Conta extends BaseController
         }
     }
 
+    // Realiza o depósito em uma conta
     public function depositar($numeroDaConta, $valor, $moeda)
     {
         $contaModel = new \App\Models\ContaModel();
@@ -136,29 +145,31 @@ class Conta extends BaseController
         if ($this->acharConta($numeroDaConta, count($contaModel->find()))) {
             if ($valor > 0) {
                 $conta = $contaModel->find($numeroDaConta);
-                $moedas = explode(" ", $conta->moedas);
-                $saldo = explode(" ", $conta->saldoMoedas);
+                $moedas = explode(" ", $conta->moedas); // string de moedas como array
+                $saldo = explode(" ", $conta->saldoMoedas); // string de saldoMoedas como array
                 $indice = 0;
                 for ($i = 0; $i < count($moedas); $i++) {
                     if ($moeda === $moedas[$i]) {
-                        $saldo[$i] += $valor;
+                        $saldo[$i] += $valor; // Altera o saldo em uma determianda moeda
                         $indice = $i;
                         break;
                     }
                 }
 
-                $conta->saldoMoedas = implode(" ", $saldo);
+                $conta->saldoMoedas = implode(" ", $saldo); // array do saldoMoedas como string novamente
 
+                // Atualiza o saldo total em reais na conta
                 if ($moeda === 'BRL') {
                     $conta->saldoTotalReais += $valor;
                 } else {
                     if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
-                        $conta->saldoTotalReais += ($valor * $this->cotacao($moedas[$indice]));
+                        $conta->saldoTotalReais += ($valor * $this->cotacao($moedas[$indice])); // Se a cotação da moeda for maior que BRL
                     } else {
-                        $conta->saldoTotalReais += ($valor / $this->cotacao($moedas[$indice]));
+                        $conta->saldoTotalReais += ($valor / $this->cotacao($moedas[$indice])); // Se a cotação da moeda for menor que BRL
                     }
                 }
 
+                // Realiza o depósito
                 if ($contaModel->update($numeroDaConta, $conta)) {
                     $this->realizarTransacao($numeroDaConta, $valor, $moeda, 'deposito');
                     echo "Depósito realizado com sucesso !\n";
@@ -173,116 +184,7 @@ class Conta extends BaseController
         }
     }
 
-    /* public function sacar($numeroDaConta, $valor, $moeda)
-    {
-        $contaModel = new \App\Models\ContaModel();
-        $valorSaque = $valor;
-
-        if ($this->acharConta($numeroDaConta, count($contaModel->find()))) {
-            if ($valor > 0) {
-                $conta = $contaModel->find($numeroDaConta);
-                if ($conta->saldoTotalReais >= $valor) {
-                    $moedas = explode(" ", $conta->moedas);
-                    $saldo = explode(" ", $conta->saldoMoedas);
-                    $indice = 0;
-                    $excecao = 0;
-                    for ($i = 0; $i < count($moedas); $i++) {
-                        if ($moeda === $moedas[$i]) {
-                            if ($saldo[$i] >= $valor) {
-                                $saldo[$i] -= $valor;
-                                $indice = $i;
-                                break;
-                            } else {
-                                $valor -= $saldo[$i];
-                                $saldo[$i] = 0;
-                                $indice = $i;
-                                $excecao = 1;
-                                break;
-                            }
-                        }
-                    }
-
-                    if ($excecao) {
-                        for ($i = 0; $i < count($moedas); $i++) {
-                            if ($saldo[$i] > 0 and $valor > 0) {
-                                if ($moedas[$i] != 'BRL') {
-                                    $saldoReal = 0;
-                                    $saldoFinal = 0;
-
-                                    if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
-                                        $saldoReal = $saldo[$i] * $this->cotacao($moedas[$i]);
-                                    } else {
-                                        $saldoReal = $saldo[$i] / $this->cotacao($moedas[$i]);
-                                    }
-
-                                    if ($moeda != 'BRL') {
-                                        if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
-                                            $saldoFinal = $saldoReal / $this->cotacao($moeda);
-                                        } else {
-                                            $saldoFinal = $saldoReal * $this->cotacao($moeda);
-                                        }
-                                        $valor -= $saldoFinal;
-                                    } else {
-                                        $valor -= $saldoReal;
-                                    }
-                                } else {
-                                    $saldoFinal = 0;
-
-                                    if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
-                                        $saldoFinal = $saldo[$i] / $this->cotacao($moeda);
-                                    } else {
-                                        $saldoFinal = $saldo[$i] * $this->cotacao($moeda);
-                                    }
-
-                                    $valor -= $saldoFinal;
-                                }
-                                if ($valor >= 0) {
-                                    $saldo[$i] = 0;
-                                } else {
-                                    $attSaldo = - ($valor);
-                                    if ($moeda === 'BRL') {
-                                        if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
-                                            $attSaldo =  $attSaldo / $this->cotacao($moedas[$i]);
-                                        } else {
-                                            $attSaldo = $attSaldo * $this->cotacao($moedas[$i]);
-                                        }
-                                    }
-                                    $saldo[$i] = $attSaldo;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    $conta->saldoMoedas = implode(" ", $saldo);
-
-                    if ($moeda === 'BRL') {
-                        $conta->saldoTotalReais -= $valorSaque;
-                    } else {
-                        if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
-                            $conta->saldoTotalReais -= ($valorSaque * $this->cotacao($moedas[$indice]));
-                        } else {
-                            $conta->saldoTotalReais -= ($valorSaque / $this->cotacao($moedas[$indice]));
-                        }
-                    }
-
-                    if ($contaModel->update($numeroDaConta, $conta)) {
-                        $this->realizarTransacao($numeroDaConta, $valorSaque, $moeda, 'saque');
-                        echo "Saque realizado com sucesso !\n";
-                    } else {
-                        echo "Não foi possível realizar o saque !\n";
-                    }
-                } else {
-                    echo "A conta não possui saldo suficiente para este saque !\n";
-                }
-            } else {
-                echo "Valor inválido !\n";
-            }
-        } else {
-            echo "Esta conta não existe !\n";
-        }
-    }*/
-
+    // Função que procura uma determinada moeda entre as disponíveis
     public function acharMoeda($moeda, $moedas)
     {
 
@@ -294,17 +196,18 @@ class Conta extends BaseController
         return -1;
     }
 
+    // Realiza o saque em uma conta, se possível
     public function sacar($numeroDaConta, $valor, $moeda)
     {
         $contaModel = new \App\Models\ContaModel();
-        $valorSaque = $valor;
+        $valorSaque = $valor; // 'valorSaque' será o valor registrado na transação, visto que a variável 'valor' será alterada
 
         if ($this->acharConta($numeroDaConta, count($contaModel->find()))) {
             if ($valor > 0) {
                 $conta = $contaModel->find($numeroDaConta);
                 $moedas = explode(" ", $conta->moedas);
-                $indice = $this->acharMoeda($moeda, $moedas);
-                $value = $valor;
+                $indice = $this->acharMoeda($moeda, $moedas); // 'indice' é a posição da moeda que será realizado o saque no array de moedas
+                $value = $valor; // 'value' é o valor total em reais do saque
                 if ($moeda != 'BRL') {
                     if ($this->verificarCotacao($this->cotacao($moedas[$indice]))) {
                         $value = $value * $this->cotacao($moedas[$indice]);
@@ -312,19 +215,20 @@ class Conta extends BaseController
                         $value = $value / $this->cotacao($moedas[$indice]);
                     }
                 }
-                if ($conta->saldoTotalReais >= $value) {
+                if ($conta->saldoTotalReais >= $value) { // Verificando se é possível realizar o saque
                     $saldo = explode(" ", $conta->saldoMoedas);
                     $excecao = 0;
-                    if ($saldo[$indice] >= $valor) {
+                    if ($saldo[$indice] >= $valor) { // Se é possível realizar o saque somente na moeda determinada
                         $saldo[$indice] -= $valor;
                     } else {
                         $valor -= $saldo[$indice];
                         $saldo[$indice] = 0;
-                        $excecao = 1;
+                        $excecao = 1; // Caindo nos outros dois casos relatados no PDF
                     }
 
                     if ($excecao) {
                         for ($i = 0; $i < count($moedas); $i++) {
+                            // Realiza as conversões de moeda e os saques
                             if ($saldo[$i] > 0 and $valor > 0) {
                                 if ($moedas[$i] != 'BRL') {
                                     $saldoReal = 0;
@@ -357,9 +261,11 @@ class Conta extends BaseController
 
                                     $valor -= $saldoFinal;
                                 }
+                                
                                 if ($valor >= 0) {
                                     $saldo[$i] = 0;
                                 } else {
+                                    // Caso tenha sido sacado valor a mais, é feito o reembolso na mesma moeda da última retirada
                                     $attSaldo = - ($valor);
                                     if ($moeda === 'BRL') {
                                         if ($this->verificarCotacao($this->cotacao($moedas[$i]))) {
@@ -377,6 +283,7 @@ class Conta extends BaseController
 
                     $conta->saldoMoedas = implode(" ", $saldo);
 
+                    // Atualiza o saldo total da conta em reais
                     if ($moeda === 'BRL') {
                         $conta->saldoTotalReais -= $valorSaque;
                     } else {
@@ -387,6 +294,7 @@ class Conta extends BaseController
                         }
                     }
 
+                    // Realiza o saque
                     if ($contaModel->update($numeroDaConta, $conta)) {
                         $this->realizarTransacao($numeroDaConta, $valorSaque, $moeda, 'saque');
                         echo "Saque realizado com sucesso !\n";
@@ -404,6 +312,7 @@ class Conta extends BaseController
         }
     }
 
+    // Exibe o saldo da conta
     public function exibirSaldo($numeroDaConta, $moeda = NULL)
     {
         $contaModel = new \App\Models\ContaModel();
@@ -413,6 +322,7 @@ class Conta extends BaseController
             $moedas = explode(" ", $conta->moedas);
             $saldo = explode(" ", $conta->saldoMoedas);
             echo "\nSaldo da conta " . $numeroDaConta . " :\n";
+            // Se não for passado nenhuma moeda como parâmetro imprime o saldo em todas as moedas
             if ($moeda === NULL) {
                 for ($i = 0; $i < count($moedas); $i++) {
                     echo $saldo[$i] . " " . $moedas[$i] . "\n";
@@ -421,6 +331,7 @@ class Conta extends BaseController
                 if ($moeda != 'BRL') {
                     $indice = 0;
                     $saldoFinal = 0;
+                    // Convertendo o saldo das moedas para BRL e depois para a moeda desejada
                     for ($i = 1; $i < count($moedas); $i++) {
                         if ($moeda === $moedas[$i]) {
                             $indice = $i;
@@ -441,6 +352,7 @@ class Conta extends BaseController
         }
     }
 
+    // Função para imprimir as informações da transação
     public function imprimirTransacoes($transacao)
     {
         echo "\n\nNumero da conta: " . $transacao->conta_numeroDaConta .
@@ -450,15 +362,18 @@ class Conta extends BaseController
             "\nData: " . $transacao->data;
     }
 
+    // Exibir todo o extrato de uma conta ou o extrato em um determinado período
     public function exibirExtrato($numeroDaConta, $inicio = NULL, $fim = NULL)
     {
         $contaModel = new \App\Models\ContaModel();
 
+        // Exibir todo o extrato da conta
         if ($inicio === NULL and $fim === NULL) {
             if ($this->acharConta($numeroDaConta, count($contaModel->find()))) {
                 $transacaoModel = new \App\Models\TransacaoModel();
-                $transacoes = $transacaoModel->find();
+                $transacoes = $transacaoModel->find(); // Acha todas as transações
                 $conta = $contaModel->find($numeroDaConta);
+                // Imprime cada uma das transações
                 foreach ($transacoes as $transacao) {
                     if ($conta->numeroDaConta === $transacao->conta_numeroDaConta) {
                         $this->imprimirTransacoes($transacao);
@@ -467,10 +382,10 @@ class Conta extends BaseController
             } else {
                 echo "Esta conta não existe !\n";
             }
-        } else if ($inicio != NULL and $fim != NULL) {
+        } else if ($inicio != NULL and $fim != NULL) { // Extrato de um determinado período
             if ($this->acharConta($numeroDaConta, count($contaModel->find()))) {
                 $aux = 0;
-                if (strtotime($inicio) > strtotime($fim)) {
+                if (strtotime($inicio) > strtotime($fim)) { // Caso os períodos estejam trocados
                     $aux = $inicio;
                     $inicio = $fim;
                     $fim = $aux;
@@ -483,6 +398,7 @@ class Conta extends BaseController
 
                 foreach ($transacoes as $transacao) {
                     if ($conta->numeroDaConta === $transacao->conta_numeroDaConta) {
+                        // Confere se a transação esta no período determinado
                         if (strtotime($inicio) < strtotime($transacao->data) and strtotime($fim) > strtotime($transacao->data)) {
                             $aux = 1;
                             $this->imprimirTransacoes($transacao);
